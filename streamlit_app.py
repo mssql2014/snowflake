21313ec2b4697dab8d2ee561399bb30b1ee11543
Dmitry Braverman <51179674+mssql2014@users.noreply.github.com>
2022-07-10 14:43:24 -0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -19,4 +19,6 @@ fruits_to_show = my_fruit_list.loc[fruits_selected]
 
 streamlit.dataframe(fruits_to_show)
 streamlit.header("Fruityvice Fruit Advice!")
-streamlit.text(fruityvice_response.json())
+df = fruityvice_response.json()
+fruityvice_normalized = pandas.json_normalize(df)
+streamlit.dataframe(df)
