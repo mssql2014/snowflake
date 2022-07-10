b087484dbcd8bfa14d4f30b34a2ccf67c043ecaf
Dmitry Braverman <51179674+mssql2014@users.noreply.github.com>
2022-07-10 14:38:06 -0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -18,4 +18,4 @@ fruits_selected=streamlit.multiselect("Pick some fruits:", list(my_fruit_list.in
 fruits_to_show = my_fruit_list.loc[fruits_selected]
 
 streamlit.dataframe(fruits_to_show)
-streamlit.text(fruityvice_response.jason)
+streamlit.text(fruityvice_response.json)
