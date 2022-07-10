dca46de98ae634d813847cae9b9921a0e507bb49
Dmitry Braverman <51179674+mssql2014@users.noreply.github.com>
2022-07-10 14:49:57 -0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,9 +2,6 @@ import streamlit
 import pandas
 import requests
 
-fruityvice_response = requests.get("https://fruityvice.com/api/fruit/"+"kiwi")
-
-
 streamlit.title('Italy is great')
 streamlit.header('Breakfast Favorites')
 streamlit.text('🥣 Omega 3 & Blueberry Oatmeal')
@@ -21,7 +18,7 @@ streamlit.dataframe(fruits_to_show)
 streamlit.header("Fruityvice Fruit Advice!")
 fruit_choice = streamlit.text_input('What fruit would you like information about?','Kiwi')
 streamlit.write('The user entered ', fruit_choice)
-
+fruityvice_response = requests.get("https://fruityvice.com/api/fruit/"+fruit_choice)
 df = fruityvice_response.json()
 fruityvice_normalized = pandas.json_normalize(df)
 streamlit.dataframe(df)
