80873cbc02437acf3db872ebb663f28e9431e651
Dmitry Braverman <51179674+mssql2014@users.noreply.github.com>
2022-07-11 20:45:23 -0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -19,6 +19,10 @@ streamlit.dataframe(fruits_to_show)
 streamlit.header("Fruityvice Fruit Advice!")
 fruit_choice = streamlit.text_input('What fruit would you like information about?','Kiwi')
 streamlit.write('The user entered ', fruit_choice)
+add_my_fruit = streamlit.text_input('What fruit would you like to add')
+streamlit.write('Thank you for adding ', add_my_fruit)
+
+
 fruityvice_response = requests.get("https://fruityvice.com/api/fruit/"+fruit_choice)
 df = fruityvice_response.json()
 fruityvice_normalized = pandas.json_normalize(df)
